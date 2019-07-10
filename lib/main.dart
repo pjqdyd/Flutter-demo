9b9e8e5daf0cda2bef9d4aa770e5bc4730f19112
pjqdyd <1909025079@qq.com>
2019-07-10 18:58:05 +0800

    网格GridView.count布局

--- lib/main.dart
+++ lib/main.dart
@@ -20,24 +20,41 @@ class MyApp extends StatelessWidget {
 
 //主页内容区组件
 class HomeContent extends StatelessWidget {
-  //定义一个List对象数组
-  List listData = [
-    {"title": "标题一", "author": "作者1", "imageUrl": "images/face.jpg"},
-    {"title": "标题二", "author": "作者2", "imageUrl": "images/face.jpg"},
-    {"title": "标题三", "author": "作者3", "imageUrl": "images/face.jpg"}
-  ];
+  /*
+   * 定义一个返回容器数组的方法
+   */
+  List<Widget> _getListData() {
+    List<Widget> list = new List();
+    //循环添加20个容器
+    for (var i = 0; i < 20; i++) {
+      list.add(Container(
+        alignment: Alignment.center,
+        color: Colors.black,
+        child: Text(
+          "这是第 $i 条数据",
+          style: TextStyle(color: Colors.blue, fontSize: 20),
+        ),
+      ));
+    }
+    return list;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder( //使用ListView的builder方法来生成列表
-      itemCount: listData.length,
-      itemBuilder: (context, index){
-        return ListTile(
-          leading: Image.asset(listData[index]["imageUrl"]),
-          title: Text(listData[index]["title"]),
-          subtitle: Text(listData[index]["author"]),
-        );
-      },
+    return GridView.count(
+      crossAxisCount: 2, //分成两列布局
+      crossAxisSpacing: 20, //列间距
+      mainAxisSpacing: 20,  //行间距
+      padding: EdgeInsets.all(10), //内边距
+      childAspectRatio: 0.7, //单个网格元素的宽高比
+      // children: <Widget>[
+      //   Text("HELLO"),
+      //   Text("HELLO"),
+      //   Text("HELLO"),
+      //   Text("HELLO"),
+      //   Text("HELLO"),
+      // ],
+      children: this._getListData()
     );
   }
 }
