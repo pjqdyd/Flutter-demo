7c87dcf595e304083cb8e83c37e8edf6345259c0
pjqdyd <1909025079@qq.com>
2019-07-11 15:40:25 +0800

    网格图片布局

--- lib/main.dart
+++ lib/main.dart
@@ -20,41 +20,49 @@ class MyApp extends StatelessWidget {
 
 //主页内容区组件
 class HomeContent extends StatelessWidget {
-  /*
-   * 定义一个返回容器数组的方法
-   */
+  //定义一个List对象数组
+  List listData = [
+    {"title": "标题一", "author": "作者1", "imageUrl": "images/face.jpg"},
+    {"title": "标题二", "author": "作者2", "imageUrl": "images/face.jpg"},
+    {"title": "标题三", "author": "作者3", "imageUrl": "images/face.jpg"}
+  ];
+
+  //返回容器列表组件的方法
   List<Widget> _getListData() {
-    List<Widget> list = new List();
-    //循环添加20个容器
-    for (var i = 0; i < 20; i++) {
-      list.add(Container(
+    var tempList = listData.map((value) {
+      return Container(
         alignment: Alignment.center,
-        color: Colors.black,
-        child: Text(
-          "这是第 $i 条数据",
-          style: TextStyle(color: Colors.blue, fontSize: 20),
+        decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+        child: Column(  //列布局,与ListView不同的是,不会固定大小
+          children: <Widget>[
+            Image.asset(value['imageUrl']),
+            Text(
+              value['title'],
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 10),
+            )
+          ],
         ),
-      ));
-    }
-    return list;
+      );
+    });
+    return tempList.toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return GridView.count(
-      crossAxisCount: 2, //分成两列布局
-      crossAxisSpacing: 20, //列间距
-      mainAxisSpacing: 20,  //行间距
-      padding: EdgeInsets.all(10), //内边距
-      childAspectRatio: 0.7, //单个网格元素的宽高比
-      // children: <Widget>[
-      //   Text("HELLO"),
-      //   Text("HELLO"),
-      //   Text("HELLO"),
-      //   Text("HELLO"),
-      //   Text("HELLO"),
-      // ],
-      children: this._getListData()
-    );
+        crossAxisCount: 2, //分成两列布局
+        crossAxisSpacing: 20, //列间距
+        mainAxisSpacing: 20, //行间距
+        padding: EdgeInsets.all(10), //内边距
+        //childAspectRatio: 0.7, //单个网格元素的宽高比
+        // children: <Widget>[
+        //   Text("HELLO"),
+        //   Text("HELLO"),
+        //   Text("HELLO"),
+        //   Text("HELLO"),
+        //   Text("HELLO"),
+        // ],
+        children: this._getListData());
   }
 }
