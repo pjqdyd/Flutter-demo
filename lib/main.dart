ae9b0fe05c719804757f1117f8d4d9e30539bf0c
pjqdyd <1909025079@qq.com>
2019-08-01 13:19:29 +0800

    定位布局

--- lib/main.dart
+++ lib/main.dart
@@ -19,62 +19,87 @@ class MyApp extends StatelessWidget {
 }
 
 //主页内容区组件
+// class HomeContent extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Stack(
+//         //alignment: Alignment.center, //所有子组件的对齐方式(位置)
+//         alignment: Alignment(0, 0),
+//         children: <Widget>[
+//           Container(
+//             height: 400,
+//             width: 300,
+//             color: Colors.red,
+//           ),
+//           Text(
+//             "这是文本",
+//             style: TextStyle(fontSize: 40, color: Colors.white),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+//主页内容区组件
+// class HomeContent extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Container(
+//         height: 400,
+//         width: 300,
+//         color: Colors.red,
+//         child: Stack(
+//           //alignment: Alignment.center, //所有子元素图标会重叠在中心
+//           children: <Widget>[
+//             Align(
+//               alignment: Alignment.topLeft,
+//               child: Icon(Icons.home, size: 40, color: Colors.white),
+//             ),
+//             Align(
+//               alignment: Alignment.center,
+//               child: Icon(Icons.search, size: 30, color: Colors.white),
+//             ),
+//             Align(
+//               alignment: Alignment.bottomRight,
+//               child: Icon(Icons.send, size: 60, color: Colors.orange),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//主页内容区组件
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-
-        Row(
+    return Center(
+      child: Container(
+        height: 400,
+        width: 300,
+        color: Colors.red,
+        child: Stack(
           children: <Widget>[
-            Expanded(
-              child: Container(
-                height: 180,
-                color: Colors.black,
-                child: Text("Hello Flutter", textAlign: TextAlign.center,),
-              ),
-            )
+            Positioned(
+              left: 10,
+              child: Icon(Icons.home, size: 40, color: Colors.white),
+            ),
+             Positioned(
+              bottom: 0,
+              left: 100,
+              child: Icon(Icons.search, size: 30, color: Colors.white),
+            ),
+             Positioned(
+              right: 0,
+              child: Icon(Icons.send, size: 60, color: Colors.orange),
+            ),
           ],
         ),
-
-        SizedBox(height: 10),
-
-        Row(
-          children: <Widget>[
-
-            Expanded(
-              flex: 2,
-              child: Container(
-                height: 180,
-                child: Image.asset("images/face.jpg", fit: BoxFit.cover),
-              ),
-            ),
-
-            SizedBox(width: 10),
-
-            Expanded(
-              flex: 1,
-              child: Container(
-                height: 180,
-                child: ListView(
-                  children: <Widget>[
-                    Container(
-                      height: 85,
-                      child: Image.asset("images/face.jpg", fit: BoxFit.cover),
-                    ),
-                    SizedBox(height: 10),
-                    Container(
-                      height: 85,
-                      child: Image.asset("images/face.jpg", fit: BoxFit.cover),
-                    )
-                  ],
-                ),
-              ),
-            )
-
-          ],
-        )
-      ],
+      ),
     );
   }
 }
