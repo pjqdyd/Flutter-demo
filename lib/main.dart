c36a8e23ee6bfe4222a8fb55b86f4e8813f0b5f7
pjqdyd <1909025079@qq.com>
2019-08-01 14:13:23 +0800

    Card布局

--- lib/main.dart
+++ lib/main.dart
@@ -18,56 +18,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//主页内容区组件
+// //主页内容区组件
 // class HomeContent extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
-//     return Center(
-//       child: Stack(
-//         //alignment: Alignment.center, //所有子组件的对齐方式(位置)
-//         alignment: Alignment(0, 0),
-//         children: <Widget>[
-//           Container(
-//             height: 400,
-//             width: 300,
-//             color: Colors.red,
-//           ),
-//           Text(
-//             "这是文本",
-//             style: TextStyle(fontSize: 40, color: Colors.white),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-//主页内容区组件
-// class HomeContent extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Container(
-//         height: 400,
-//         width: 300,
-//         color: Colors.red,
-//         child: Stack(
-//           //alignment: Alignment.center, //所有子元素图标会重叠在中心
-//           children: <Widget>[
-//             Align(
-//               alignment: Alignment.topLeft,
-//               child: Icon(Icons.home, size: 40, color: Colors.white),
-//             ),
-//             Align(
-//               alignment: Alignment.center,
-//               child: Icon(Icons.search, size: 30, color: Colors.white),
-//             ),
-//             Align(
-//               alignment: Alignment.bottomRight,
-//               child: Icon(Icons.send, size: 60, color: Colors.orange),
-//             ),
-//           ],
-//         ),
+//     return Container(
+//       width: 300,
+//       child: AspectRatio(   //配置子元素的宽高比
+//         aspectRatio: 2.0/1.0,
+//         child: Container(
+//           color: Colors.red,
+//         )
 //       ),
 //     );
 //   }
@@ -77,29 +38,52 @@ class MyApp extends StatelessWidget {
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        height: 400,
-        width: 300,
-        color: Colors.red,
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              left: 10,
-              child: Icon(Icons.home, size: 40, color: Colors.white),
-            ),
-             Positioned(
-              bottom: 0,
-              left: 100,
-              child: Icon(Icons.search, size: 30, color: Colors.white),
-            ),
-             Positioned(
-              right: 0,
-              child: Icon(Icons.send, size: 60, color: Colors.orange),
-            ),
-          ],
+    return ListView(
+      children: <Widget>[
+        Card(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+
+              ListTile(
+                title: Text("张三", style: TextStyle(fontSize: 28)),
+                subtitle: Text("这是展示信息"),
+              ),
+              ListTile(
+                title: Text("电话", style: TextStyle(fontSize: 28)),
+                subtitle: Text("这是展示信息"),
+              ),
+              ListTile(
+                title: Text("地址", style: TextStyle(fontSize: 28)),
+                subtitle: Text("这是展示信息"),
+              ),
+
+            ],
+          )
         ),
-      ),
+
+        Card(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+
+              ListTile(
+                title: Text("王五", style: TextStyle(fontSize: 28)),
+                subtitle: Text("这是展示信息"),
+              ),
+              ListTile(
+                title: Text("电话", style: TextStyle(fontSize: 28)),
+                subtitle: Text("这是展示信息"),
+              ),
+              ListTile(
+                title: Text("地址", style: TextStyle(fontSize: 28)),
+                subtitle: Text("这是展示信息"),
+              ),
+
+            ],
+          )
+        ),
+      ],
     );
   }
 }
