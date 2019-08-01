a37178ac91e256c5ab475416c5a22d596fcd40c4
pjqdyd <1909025079@qq.com>
2019-08-01 15:48:53 +0800

    Wrap自动换行流布局

--- lib/main.dart
+++ lib/main.dart
@@ -20,39 +20,46 @@ class MyApp extends StatelessWidget {
 
 //主页内容区组件
 class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 600,
+      alignment: Alignment.topCenter,
+      child: Wrap(
+        spacing: 10, //子组件主轴方向的间距
+        alignment: WrapAlignment.center, //主轴上的对齐方式
+        runSpacing: 10, //纵轴的间距
+        //runAlignment: WrapAlignment.end, //纵轴的对齐方式
+        direction: Axis.vertical, //纵轴排列
+        children: <Widget>[
+          MyButton("第一季"),
+          MyButton("第二季"),
+          MyButton("第三季"),
+          MyButton("第四季"),
+          MyButton("第五季"),
+          MyButton("第六季(热门)"),
+          MyButton("第七季"),
+          MyButton("第八季(最新)"),
+        ],
+      ),
+    );
+  }
+}
 
-  //定义一个List对象数组
-  List listData = [
-    {"title": "标题一", "author": "作者1", "imageUrl": "images/face.jpg"},
-    {"title": "标题二", "author": "作者2", "imageUrl": "images/face.jpg"},
-    {"title": "标题三", "author": "作者3", "imageUrl": "images/face.jpg"}
-  ];
+//自定义按钮组件
+class MyButton extends StatelessWidget {
+  //不能修改, 由构造器初始化
+  final String text;
+
+  //text为必传, key是当前组件的key, 也可不先初始化父类
+  const MyButton(this.text, {Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: listData.map((value){ //循环数据, 渲染卡片
-
-        return Card(
-          margin: EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              AspectRatio(
-                aspectRatio: 16 / 9,
-                child: Image.asset(value["imageUrl"], fit: BoxFit.cover),
-              ),
-              ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage(value["imageUrl"]),
-                ),
-                title: Text(value["title"]),
-                subtitle: Text(value["author"], maxLines: 2, overflow: TextOverflow.ellipsis,),
-              )
-            ],
-          ),
-        );
-
-      }).toList(),
+    return RaisedButton(
+      child: Text(this.text),
+      textColor: Theme.of(context).accentColor,
+      onPressed: () {},
     );
   }
 }
