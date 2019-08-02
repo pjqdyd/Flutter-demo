1086d5c7a54cdac2f751a3ba8f7993ebf0513cc8
pjqdyd <1909025079@qq.com>
2019-08-02 13:11:14 +0800

    --add-

---
+++ lib/pages/Search.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+/** 
+ * 搜索页面
+*/
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("搜索页面")
+      ),
+      body: Text("搜索页面内容区域"),
+    );
+  }
+}
