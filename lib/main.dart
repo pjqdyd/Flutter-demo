bd489b86f990e5c6334214d17e150e405d8b1d7d
pjqdyd <1909025079@qq.com>
2019-08-02 13:10:43 +0800

    路由跳转

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'pages/Search.dart';
+
 void main() => runApp(MyApp());
 
 //MyApp组件
@@ -7,50 +9,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyTabs(), //展示Tab栏
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter"),
+        ),
+        body: HomePage(),
+      ),
       //theme: ThemeData(primarySwatch: Colors.yellow),
     );
   }
 }
 
-//自定义组件, 将主页内容Scaffold抽离成有状态组件 (展示Tab栏)
-class MyTabs extends StatefulWidget {
-  MyTabs({Key key}) : super(key: key);
+//自定义主页组件
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
 
-  _MyTabsState createState() => _MyTabsState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyTabsState extends State<MyTabs> {
-  int _currentIndex = 0;
-
-  //定义页面的集合, Text()代表是一个单独的页面组件
-  List _pageList = [
-    Text("这是主页组件"),
-    Text("这是分类组件"),
-    Text("这是设置组件"),
-  ];
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter"),
-      ),
-      body: this._pageList[this._currentIndex], //body为对应的页面
-      bottomNavigationBar: BottomNavigationBar(//底部选项卡
-        iconSize: 30.0,
-        currentIndex: this._currentIndex,
-        onTap: (int index) { //点击改变激活的下标
-          setState(() {
-            this._currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("首页")),
-          BottomNavigationBarItem(icon: Icon(Icons.category), title: Text("分类")),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("设置"))
-        ],
-      ),
+    return Center(
+      child: RaisedButton(
+            child: Text("跳转到搜索页面"),
+            onPressed: (){
+              Navigator.of(context).push( //路由跳转
+                MaterialPageRoute(
+                  builder: (context) => SearchPage()
+                )
+              );
+            },
+          )
     );
   }
 }
