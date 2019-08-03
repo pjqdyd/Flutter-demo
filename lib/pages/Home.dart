2f7211d9941c067e6929e30ed0264ab91ea966c2
pjqdyd <1909025079@qq.com>
2019-08-03 15:39:59 +0800

    顶部TapBar选项卡

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -10,31 +10,57 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home主页"),
+    // return DefaultTabController( //顶部选项卡
+    //   length: 2,      //顶部切换的数量
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text("Home主页"),
+    //       bottom: TabBar(  //放在title:上,选项卡在顶部就会替换标题
+    //         tabs: <Widget>[
+    //           Tab(text: "热门"),
+    //           Tab(text: "推荐")
+    //         ],
+    //       ),
+    //     ),
+    //     body: TabBarView(  //对应上面的TabBar
+    //       children: <Widget>[
+    //         Text("第一个Tab"),
+    //         Text("第二个Tab"),
+    //       ],
+    //     )
+    //   ),
+    // );
 
-        //centerTitle: true,
-
-        //backgroundColor: Colors.green,
-
-        //leading: Icon(Icons.menu),  //左侧普通图标
-
-        leading: IconButton(  //左侧按钮图标
-          icon: Icon(Icons.menu),
-          onPressed: () {},
-        ),
-
-        actions: <Widget>[ //右侧图标组
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
+    return DefaultTabController(
+      length: 3, //顶部切换的数量
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(  //顶部选项卡, 放在顶部
+            children: <Widget>[
+              Expanded(
+                child: TabBar(
+                  indicatorColor: Colors.red, //底部指示器颜色
+                  indicatorSize: TabBarIndicatorSize.label, //指示器宽度(与lable等长)
+                  labelColor: Colors.red,     //选项颜色
+                  unselectedLabelColor: Colors.white, //未选中颜色
+                  tabs: <Widget>[
+                    Tab(text: "热门"),
+                    Tab(text: "推荐"),
+                    Tab(text: "最新"),
+                  ],
+                ),
+              )
+            ],
           ),
-        ],
-      ),
-
-      body: Center(
-        child: Text("主页"),
+        ),
+        body: TabBarView(
+          //对应上面的TabBar
+          children: <Widget>[
+            Text("第一个Tab"),
+            Text("第二个Tab"),
+            Text("第三个Tab"),
+          ],
+        ),
       ),
     );
   }
