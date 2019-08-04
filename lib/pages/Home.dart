1a0f3093f7b1ef6f36b08917790d82202b1dd719
pjqdyd <1909025079@qq.com>
2019-08-04 18:29:42 +0800

    底部栏 + 浮动按钮

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -8,108 +8,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  int _currentIndex = 0;
+
+  //定义页面的集合, Text()代表是一个单独的页面组件
+  List _pageList = [
+    Text("这是主页组件"),
+    Text("这是分类组件"),
+    Text("这是添加组件"),
+    Text("这是设置组件"),
+    Text("这是个人组件")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("主页"),
+      appBar: AppBar(
+        title: Text("主页"),
+      ),
+      body: this._pageList[this._currentIndex], //body为对应的页面
+
+      //浮动按钮
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.add),
+      //   onPressed: (){},
+      // ),
+      floatingActionButton: Container(
+        height: 60,
+        width: 60,
+        padding: EdgeInsets.all(6),
+        margin: EdgeInsets.only(top: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: Colors.white,
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              RaisedButton(
-                child: Text("突起按钮"),
-                color: Colors.blue,
-                textColor: Colors.white,
-                //disabledColor: ,
-                //disabledTextColor: ,
-                //splashColor: , //点击时水波纹颜色'
-                //highlightColor: , //点击长按后的颜色
-                //elevation: , //阴影范围
-                //装在Container容器中就可以设置宽高, Expanded自适应
-                onPressed: () {},
-              ),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            setState(() {
+            this._currentIndex = 2;
+          });
+          },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 
-              RaisedButton(
-                child: Text("圆角按钮"),
-                shape: RoundedRectangleBorder( //按钮形状, 圆角
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                onPressed: () {},
-              ),
-
-              RaisedButton.icon(
-                icon: Icon(Icons.settings),
-                label: Text("图标按钮"),
-                // shape: CircleBorder( //形状圆形, 边框颜色
-                //   side: BorderSide(color: Colors.white),
-                // ),
-                onPressed: () {},
-              ),
-
-              FlatButton(
-                child: Text("扁平按钮"),
-                color: Colors.blue,
-                onPressed: (){},
-              ),
-
-              OutlineButton(
-                child: Text("边框按钮"),
-                onPressed: (){},
-              ),
-
-              IconButton( //按钮图标
-                icon: Icon(Icons.send),
-                color: Colors.blue,
-                onPressed: (){},
-              ),
-
-              ButtonBar( //按钮组
-                alignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton(
-                    child: Text("按钮1"),
-                    onPressed: (){},
-                  ),
-                  RaisedButton(
-                    child: Text("按钮2"),
-                    onPressed: (){},
-                  )
-               ],
-             ),
-
-              FloatingActionButton(
-               child: Text("浮动"),
-               onPressed: (){},
-             ),
-
-              MyButton(text: "自定义按钮", height: 30, width: 120, pressed: (){print("--");},)
-
-            ],
-          ),
-        ));
-  }
-}
-
-//自定义按钮
-class MyButton extends StatelessWidget {
-  final text; //文本
-  final double height;
-  final double width;
-  final pressed; //点击函数
-
-  //按钮构造器
-  const MyButton({this.text = "", this.pressed = null, 
-  this.height = 50.0, this.width = 100.0});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: this.width,
-      height: this.height,
-      child: RaisedButton(
-        child: Text(this.text),
-        onPressed: this.pressed,
+      bottomNavigationBar: BottomNavigationBar(//底部选项卡
+        iconSize: 25.0,
+        currentIndex: this._currentIndex,
+        type: BottomNavigationBarType.fixed, //超过3个就要设置
+        selectedFontSize: 11,
+        unselectedFontSize: 10,
+        onTap: (int index) { //点击改变激活的下标
+          setState(() {
+            this._currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("首页")),
+          BottomNavigationBarItem(icon: Icon(Icons.category), title: Text("分类")),
+          BottomNavigationBarItem(icon: Icon(Icons.add), title: Text("添加")),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("设置")),
+          BottomNavigationBarItem(icon: Icon(Icons.people), title: Text("个人"))
+        ],
       ),
     );
   }
