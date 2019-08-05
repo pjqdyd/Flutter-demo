5e5ef560b0726f1ab1901594376e455a257dac7a
pjqdyd <1909025079@qq.com>
2019-08-05 17:28:24 +0800

    日期, 国际化支持

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'routes/Routes.dart';
 
+//国际化
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 void main() => runApp(MyApp());
 
 //MyApp组件
@@ -11,7 +14,17 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false, //去掉debug显示
         //home: HomePage(),
         initialRoute: '/', //初始化时加载的路由(HomePage主页)
-        onGenerateRoute: onGenerateRoute //调用Routes.dart中的可传参数方法
+        onGenerateRoute: onGenerateRoute, //调用Routes.dart中的可传参数方法
+
+        //国际化配置 (会根据设备更改语言)
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('zh', 'CH'),
+          const Locale('en', 'US')
+        ],
       );
   }
 }
--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:date_format/date_format.dart'; //第三方工具
+
 //自定义主页组件
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -9,20 +11,61 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  //文本输出框的控制值对象 (如果有初始值就要这样写)
-  var _username = new TextEditingController();
+  var now = DateTime.now(); //获取当前日期
 
-  //或者直接定义值
-  String password;
-
-  var flag = false;
-  int sex = 1;
-  int count = 1;
+  var now_time = TimeOfDay(hour: 12, minute: 20); //定义时间
 
   @override
   void initState() { 
     super.initState();
-    _username.text = "初始值";
+
+    print(now);
+
+    print(now.millisecondsSinceEpoch); //时间戳
+
+    print(DateTime.fromMillisecondsSinceEpoch(1564992543624)); //时间戳转换
+
+    //使用工具转换
+    print(formatDate(DateTime.now(), [yyyy, '年', mm, '月', dd]));
+
+    //print(now_time.format(context)); //打印时间
+  }
+
+  // //定义显示日期选框的方法
+  // _showDatePicker(context){
+  //   showDatePicker(
+  //     context: context,
+  //     initialDate: this.now,
+  //     firstDate: DateTime(1980),
+  //     lastDate: DateTime(2100),
+  //   ).then((result){ //选中日期后的回调(Future代表异步, 类似Promiss)
+  //     print(result);
+  //   });
+  // }
+
+  //定义显示日期选框的方法 (使用异步)
+  _showDatePicker(context) async{
+
+    var result = await showDatePicker(
+      context: context,
+      initialDate: this.now,
+      firstDate: DateTime(1980),
+      lastDate: DateTime(2100),
+      locale: Locale('zh')
+    );
+
+    print(result);
+
+  }
+
+  //定义显示时间的方法
+  _showTimePicker(context) async{
+     var result = await showTimePicker(
+      context: context,
+      initialTime: now_time
+    );
+
+    print(result);
   }
 
   @override
@@ -31,177 +74,28 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("主页"),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
 
-            TextField(//文本输入框（绑定值控制器）
-              controller: _username,
-              onChanged: (value){
-               setState(() {
-                  this._username.text = value;
-               });
-              },
-            ),
-            Container(
-              width: double.infinity, //自适应
-              child: RaisedButton(
-                child: Text("提交1"),
-                onPressed: (){
-                  print(this._username.text);
-                },
-              ),
-            ),
-            SizedBox(height: 10),
-
-            TextField(//文本输入框（直接绑定值）
-              onChanged: (value){
-               setState(() {
-                  this.password = value;
-               });
-              },
-            ),
-            Container(
-              width: double.infinity, //自适应
-              child: RaisedButton(
-                child: Text("提交2"),
-                onPressed: (){
-                  print(this.password);
-                },
-              ),
-            ),
-            SizedBox(height: 10),
-
-            TextField(//多行文本输入框
-              maxLength: 100,
-              maxLines: 3,
-              decoration: InputDecoration(
-                hintText: "请输入..",
-                border: OutlineInputBorder() //是否显示边框
-              ),
-            ),
-            SizedBox(height: 10),
-
-            TextField(//密码输入框
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "请输入密码..",
-              ),
-            ),
-            SizedBox(height: 10),
-
-            TextField(//标签输入框
-              decoration: InputDecoration(
-                labelText: "用户名:",
-                hintText: "请输入用户名..",
-              ),
-            ),
-            SizedBox(height: 10),
-
-            TextField(//带图标输入框
-              decoration: InputDecoration(
-                icon: Icon(Icons.people),
-                hintText: "请输入用户名..",
-              ),
-            ),
-            SizedBox(height: 10),
- 
-            Checkbox( //多选框
-              value: this.flag,
-              activeColor: Colors.yellow,
-              onChanged: (val){
-                setState(() {
-                 this.flag = val;
-                });
-              },
-            ),
-            SizedBox(height: 10),
-
-            CheckboxListTile( //多选框和ListTile结合
-              value: this.flag,
-              onChanged: (val){
-                setState(() {
-                 this.flag = val;
-                });
-              },
-              title: Text("这是标题"),
-              subtitle: Text("这是2级标题"),
-              secondary: Icon(Icons.help),
-            ),
-            SizedBox(height: 10),
-
-            Row(
+          InkWell( //可以点击的组件(有水波纹)
+            child:  Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[ //单选框 (选框要大于1个)
-                Text("男:"),
-                Radio(
-                  value: 1,
-                  groupValue: this.sex,
-                  onChanged: (val){
-                    setState(() {
-                      this.sex = val; 
-                    });
-                  },
-                ),
-                SizedBox(width: 20),
-                Text("女:"),
-                Radio(
-                  value: 2,
-                  groupValue: this.sex,
-                  onChanged: (val){
-                    setState(() {
-                      this.sex = val; 
-                    });
-                  },
-                )
+              children: <Widget>[                
+                Text("2019-08-10"),
+                Icon(Icons.arrow_drop_down)
               ],
             ),
-            SizedBox(height: 10),
+            onTap: (){
+                //打开日期组件
+                //this._showDatePicker(context);
 
-            RadioListTile(//单选框和ListTile结合(groupValue表示组)
-              value: 1,
-              groupValue: this.count,
-              title: Text("标题"),
-              subtitle: Text("子标题"),
-              secondary: Icon(Icons.flag),
-              selected: this.count == 1,
-              onChanged: (val){
-                setState(() {
-                  this.count = val; 
-                });
-              },
-            ),
-            SizedBox(width: 20),
-            RadioListTile(
-              value: 2,
-              groupValue: this.count,
-              title: Text("标题"),
-              subtitle: Text("子标题"),
-              selected: this.count == 2,
-              secondary: Icon(Icons.flag),
-              onChanged: (val){
-                setState(() {
-                  this.count = val; 
-                });
-              },
-            ),
-            SizedBox(height: 10),
-
-           Row(
-             children: <Widget>[
-              Switch( //开关
-                value: this.flag,
-                onChanged: (val){
-                  this.flag = val;
-                },
-              ),
-             ],
-           )
-
-          ],
-        ),
-      ),
+                //打开时间组件
+                this._showTimePicker(context);
+            },
+          ),
+        ],
+      )
     );
   }
 }
